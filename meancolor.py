1a932f4f6b8246b70e6a7a80f2794874470df06b
Robert Alexander <rla013@bucknell.edu>
2020-02-20 19:56:25 -0500

    transfer

--- meancolor.py
+++ meancolor.py
@@ -1,7 +1,11 @@
 import sys
 import numpy
+from scenedetect.video_splitter import  is_ffmpeg_available
 
 def meancolor():
     # find average color of frame range on video file.
     print("hello")
     print(1)
+    print(is_ffmpeg_available())
+
+meancolor()
