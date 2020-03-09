d627e067041cfa55194649a52db153ed5a478db8
jacky <mr7.jacky@gmail.com>
2020-03-09 18:03:07 -0400

    The test version of k-means main.py is a test file pic_utils contains the important part of the code testfile contains pictures from different scenes testfile-out are reshaped pictures for the faster speed of test files in cache and stats haven't been used

---
+++ ImgKMeanImg/stats/color_info.py
@@ -0,0 +1,9 @@
+'''
+Stats data of a image
+'''
+
+
+class color_info:
+
+    def __init__(self):
+        self.info
---
+++ ImgKMeanImg/stats/img_stats.py
@@ -0,0 +1,12 @@
+'''
+The class contains the data of img k-means cluster
+'''
+
+
+class img_stats:
+
+    def __init__(self, film, begin_time, end_time, color_info):
+        self.film = film
+        self.begin_time = begin_time
+        self.end_time = end_time
+        self.color_info = color_info
