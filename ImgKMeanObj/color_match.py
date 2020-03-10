e4b4b873f4293284eb9a74571008475aca026f82
jacky <mr7.jacky@gmail.com>
2020-03-10 11:25:46 -0400

    new function to calculate the weighted color difference of two img

--- ImgKMeanObj/color_match.py
+++ ImgKMeanObj/color_match.py
@@ -1,17 +1,54 @@
 import cv2
 import numpy as np
-from skimage.color import rgb2lab, deltaE_cie76
+import math
 
 
-def match_image_by_color(image, color, threshold=60, number_of_colors=10):
-    image_colors = get_colors(image, number_of_colors, False)
-    selected_color = rgb2lab(np.uint8(np.asarray([[color]])))
+def cal_diff(ctr_img, cmp_img):
+    """
+    The function to compare the difference between two images from the most dominant color
+    The calculation is based on the Delta CIE76.
+    The formula for individual color difference is diff = sqrt((R1-R2)^2+(G1-G2)^2+(B1-B2)^2)
+    We add the percentage contains in the certain color to generate the weighted difference.
+    Given that the color information for each image is on descending order based on the dominance
+    of the color, we calculate the most significant color first.
+    :param ctr_img: The control group of the image, it is usually the image user upload
+    :param cmp_img: The image to compare with the control group.
+    :return: weighted_diff: a float weighted number based on the difference between two image
+    """
+    weighted_diff = 0
+    for i in range(len(cmp_img.color_info)):
+        # calculate the difference of the color distribution
+        RGB_diff = np.array(cmp_img.color_info[i].RGB) - np.array(ctr_img.color_info[i].RGB)
+        diff = math.sqrt(np.sum(RGB_diff ** 2))
+        print(diff, cmp_img.color_info[i].percent)
+        weighted_diff += diff * cmp_img.color_info[i].percent
+    return weighted_diff
 
-    select_image = False
-    for i in range(number_of_colors):
-        curr_color = rgb2lab(np.uint8(np.asarray([[image_colors[i]]])))
-        diff = deltaE_cie76(selected_color, curr_color)
-        if (diff < threshold):
-            select_image = True
 
-    return select_image
+def cal_diff_treshold(ctr_img, cmp_img, threshold=100):
+    """
+    The function to compare the difference between two images from the most dominant color
+    The calculation is based on the Delta CIE76.
+    The formula for individual color difference is diff = sqrt((R1-R2)^2+(G1-G2)^2+(B1-B2)^2)
+    We add the percentage contains in the certain color to generate the weighted difference.
+    Given that the color information for each image is on descending order based on the dominance
+    of the color, we calculate the most significant color first.
+    :param threshold: the percentage of color difference to compare, it should be a number from 0 to 100 and
+    the default value is 100
+    :param ctr_img: The control group of the image, it is usually the image user upload
+    :param cmp_img: The image to compare with the control group.
+    :return: weighted_diff: a float weighted number based on the difference between two image
+    """
+    threshold = threshold / 100
+    weighted_diff = 0
+    calculated_percent = 0
+    for i in range(len(cmp_img.color_info)):
+        # calculate the difference of the color distribution
+        RGB_diff = np.array(cmp_img.color_info[i].RGB) - np.array(ctr_img.color_info[i].RGB)
+        diff = math.sqrt(np.sum(RGB_diff ** 2))
+        calculated_percent += cmp_img.color_info[i].percent
+        if calculated_percent >= threshold:
+            break
+        print(diff, cmp_img.color_info[i].percent)
+        weighted_diff += diff * cmp_img.color_info[i].percent
+    return weighted_diff
--- ImgKMeanObj/main.py
+++ ImgKMeanObj/main.py
@@ -60,17 +60,26 @@ def reshape_data_setup():
         picU.kmeans_cluster(filename, 10, True, out2)
 
 
-# compare the difference between two images from the most dominant color
-def cal_diff(img, img1, tolerance):
-    percentage = 0
-    for i in range(len(img1.color_info)):
-        RGB_diff = np.array(img1.color_info[i].RGB) - np.array(img.color_info[i].RGB)
+def cal_diff(ctr_img, cmp_img):
+    """
+    The function to compare the difference between two images from the most dominant color
+    The calculation is based on the Delta CIE76.
+    The formula for individual color difference is diff = sqrt((R1-R2)^2+(G1-G2)^2+(B1-B2)^2)
+    We add the percentage contains in the certain color to generate the weighted difference.
+    Given that the color information for each image is on descending order based on the dominance
+    of the color, we calculate the most significant color first.
+    :param ctr_img: The control group of the image, it is usually the image user upload
+    :param cmp_img: The image to compare with the control group.
+    :return: weighted_diff: a float weighted number based on the difference between two image
+    """
+    weighted_diff = 0
+    for i in range(len(cmp_img.color_info)):
+        # calculate the difference of the color distribution
+        RGB_diff = np.array(cmp_img.color_info[i].RGB) - np.array(ctr_img.color_info[i].RGB)
         diff = math.sqrt(np.sum(RGB_diff ** 2))
-        if (diff < 10 and percentage >= tolerance):
-            return True
-        elif diff > 10 and percentage < tolerance:
-            return False
-    return True
+        print(diff, cmp_img.color_info[i].percent)
+        weighted_diff += diff * cmp_img.color_info[i].percent
+    return weighted_diff
 
 
 def search(filename):
